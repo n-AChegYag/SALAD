a939ae3b139353b2eefa4b5af6702e7f1c454936
acy_ps <190536889@qq.com>
2022-06-15 01:25:35 +0800

    add models, losses and dataset

---
+++ src/eye_dataset.py
@@ -0,0 +1,54 @@
+import os
+from random import random
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class EyeDataset(Dataset):
+
+    def __init__(self, data_path, mode='train'):
+        super().__init__()
+        self.data_path = data_path
+        self.all_images = [os.path.join(data_path, sample) for sample in os.listdir(data_path)]
+        self.mode = mode
+
+    def __len__(self):
+        return len(self.all_images)
+
+    def __getitem__(self, index):
+        image = cv2.imread(self.all_images[index])[:,:,0]
+        image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_LANCZOS4)
+        flag = 0
+        if self.mode == 'train':
+            r = np.random.rand()
+            if r >= 0.5:
+                flag = 1
+                image = self._in_painting(image)
+        image = image / 255.0
+        image = image[:,:,np.newaxis]
+        return image.transpose(2,0,1), flag
+
+    @staticmethod
+    def _in_painting(image, patch_num=5, patch_range=(1/8,1/5)):
+        W, H = image.shape
+        min_size = (W*patch_range[0], H*patch_range[0])
+        max_size = (W*patch_range[1], H*patch_range[1])
+        for _ in range(patch_num):
+            x = np.random.randint(max_size[0], W-max_size[0])
+            y = np.random.randint(max_size[1], H-max_size[1])
+            patch_w = np.random.randint(min_size[0]/2, max_size[0]/2)
+            patch_h = np.random.randint(min_size[1]/2, max_size[1]/2)
+            patch_edge= [x-patch_w, x+patch_w, y-patch_h, y+patch_h]
+            noise = np.random.randn(patch_edge[1]-patch_edge[0], patch_edge[3]-patch_edge[2]) * 255
+            image[patch_edge[0]:patch_edge[1], patch_edge[2]: patch_edge[3]] = noise
+        return image
+
+
+if __name__ == '__main__':
+
+    from torch.utils.data import DataLoader
+    eye_dataset = EyeDataset('/home/ps/disk12t/ACY/SALAD/data/eye/A')
+    eye_dataloader = DataLoader(eye_dataset, batch_size=4, drop_last=True)
+    for image, flag in eye_dataloader:
+        print(image.shape, flag)
---
+++ src/models.py
@@ -0,0 +1,292 @@
+import torch.nn as nn
+import torch
+
+##############################
+#        Blocks
+##############################
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+class Down(nn.Module):
+    def __init__(self, in_size, out_size, normalize=True, dropout=0.0):
+        super(Down, self).__init__()
+        layers = [nn.Conv2d(in_size, out_size, 4, 2, 1, bias=False)]
+        if normalize:
+            layers.append(nn.InstanceNorm2d(out_size))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_size, out_size, dropout=0.0):
+        super(Up, self).__init__()
+        layers = [
+            nn.ConvTranspose2d(in_size, out_size, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(out_size),
+            nn.LeakyReLU(0.2, inplace=True),
+        ]
+        if dropout:
+            layers.append(nn.Dropout(dropout))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+##############################
+#        Generator
+##############################
+
+class ME(nn.Module):
+
+    def __init__(self, in_channels=1, out_channels=512):
+        super().__init__()
+
+        self.down1 = Down(in_channels, 64, normalize=False)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512, dropout=0.5)
+        self.down5 = Down(512, 512, dropout=0.5)
+        self.down6 = Down(512, 512, dropout=0.5)
+        self.down7 = Down(512, 512, dropout=0.5)
+        self.down8 = Down(512, out_channels, normalize=False, dropout=0.5)
+
+    def forward(self, x):
+
+        x = self.down1(x)       # 256 -> 128
+        x = self.down2(x)       # 128 -> 64
+        x = self.down3(x)       # 64 -> 32
+        x = self.down4(x)       # 32 -> 16
+        x = self.down5(x)       # 16 -> 8
+        x = self.down6(x)       # 8 -> 4
+        x = self.down7(x)       # 4 -> 2
+        x = self.down8(x)       # 2 -> 1
+
+        return x
+
+
+class MD(nn.Module):
+
+    def __init__(self, in_channels=512, out_channels=1):
+        super().__init__()
+
+        self.up1 = Up(in_channels, 512, dropout=0.5)
+        self.up2 = Up(512, 512, dropout=0.5)
+        self.up3 = Up(512, 512, dropout=0.5)
+        self.up4 = Up(512, 512, dropout=0.5)
+        self.up5 = Up(512, 256)
+        self.up6 = Up(256, 128)
+        self.up7 = Up(128, 64)
+        self.up8 = Up(64, 1)
+
+    def forward(self, x):
+
+        x = self.up1(x)       # 1 -> 2
+        x = self.up2(x)       # 2 -> 4
+        x = self.up3(x)       # 4 -> 8
+        x = self.up4(x)       # 8 -> 16
+        x = self.up5(x)       # 16 -> 32
+        x = self.up6(x)       # 32 -> 64
+        x = self.up7(x)       # 64 -> 128
+        x = self.up8(x)       # 128 -> 256
+
+        return x
+
+
+class IpNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):
+        super(IpNet, self).__init__()
+
+        self.down1 = Down(in_channels, 64, normalize=False)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512, dropout=0.5)
+        self.down5 = Down(512, 512, dropout=0.5)
+        self.down6 = Down(512, 512, dropout=0.5)
+        self.down7 = Down(512, 512, dropout=0.5)
+        self.down8 = Down(512, 512, normalize=False, dropout=0.5)
+
+        self.up1 = Up(512, 512, dropout=0.5)
+        self.up2 = Up(512, 512, dropout=0.5)
+        self.up3 = Up(512, 512, dropout=0.5)
+        self.up4 = Up(512, 512, dropout=0.5)
+        self.up5 = Up(512, 256)
+        self.up6 = Up(256, 128)
+        self.up7 = Up(128, 64)
+
+        self.final = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(64, out_channels, 4, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+
+        x = self.down1(x)       # 256 -> 128
+        x = self.down2(x)       # 128 -> 64
+        x = self.down3(x)       # 64 -> 32
+        x = self.down4(x)       # 32 -> 16
+        x = self.down5(x)       # 16 -> 8
+        x = self.down6(x)       # 8 -> 4
+        x = self.down7(x)       # 4 -> 2
+        z_p = self.down8(x)     # 2 -> 1
+        x = self.up1(z_p)       # 1 -> 2
+        x = self.up2(x)         # 2 -> 4
+        x = self.up3(x)         # 4 -> 8
+        x = self.up4(x)         # 8 -> 16
+        x = self.up5(x)         # 16 -> 32
+        x = self.up6(x)         # 32 -> 64
+        x = self.up7(x)         # 64 -> 128
+        x = self.final(x)       # 128 -> 256
+
+        return x, z_p
+
+
+class FpNet(nn.Module):
+    def __init__(self, in_channels=512, out_channels=512):
+        super(FpNet, self).__init__()
+
+        self.up1 = Up(in_channels, 512, dropout=0.5)
+        self.up2 = Up(512, 512, dropout=0.5)
+        self.up3 = Up(512, 512, dropout=0.5)
+        self.up4 = Up(512, 512, dropout=0.5)
+        self.up5 = Up(512, 256)
+        self.up6 = Up(256, 128)
+        self.up7 = Up(128, 64)
+        self.up8 = Up(64, 1)
+
+        self.down1 = Down(1, 64, normalize=False)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512, dropout=0.5)
+        self.down5 = Down(512, 512, dropout=0.5)
+        self.down6 = Down(512, 512, dropout=0.5)
+        self.down7 = Down(512, 512, dropout=0.5)
+        self.down8 = Down(512, out_channels, normalize=False, dropout=0.5)
+
+    def forward(self, z):
+
+        z = self.up1(z)         # 1 -> 2
+        z = self.up2(z)         # 2 -> 4
+        z = self.up3(z)         # 4 -> 8
+        z = self.up4(z)         # 8 -> 16
+        z = self.up5(z)         # 16 -> 32
+        z = self.up6(z)         # 32 -> 64
+        z = self.up7(z)         # 64 -> 128
+        x_p = self.up8(z)       # 128 -> 256
+
+        z = self.down1(x_p)     # 256 -> 128
+        z = self.down2(z)       # 128 -> 64
+        z = self.down3(z)       # 64 -> 32
+        z = self.down4(z)       # 32 -> 16
+        z = self.down5(z)       # 16 -> 8
+        z = self.down6(z)       # 8 -> 4
+        z = self.down7(z)       # 4 -> 2
+        z = self.down8(z)     # 2 -> 1
+
+        return z, x_p
+
+
+##############################
+#        Discriminator
+##############################
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=1):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, normalization=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalization:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels, 64, normalization=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(512, 1, 4, padding=1, bias=False)
+        )
+
+    def forward(self, img):
+        return self.model(img)
+
+
+class MLP(nn.Module):
+    def __init__(self, in_channels=512):
+        super(MLP, self).__init__()
+
+        self.input_fc = nn.Linear(in_channels, 256)
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.hidden_fc = nn.Linear(256, 128)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+        self.output_fc = nn.Linear(128, 1)
+
+    def forward(self, x):
+
+        x = self.input_fc(x)
+        x = self.relu1(x)
+        x = self.hidden_fc(x)
+        x = self.relu2(x)
+        x = self.output_fc(x)
+
+        return x
+
+
+if __name__ == '__main__':
+
+    import os
+    import numpy as np
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
+    m_e = ME()
+    m_d = MD()
+    d_i = Discriminator(1)
+    d_f = MLP(512)
+
+    m_e.cuda()
+    m_d.cuda()
+    d_i.cuda()
+    d_f.cuda()
+
+    x = np.zeros((2, 1, 256, 256)).astype('float32')
+    x = torch.from_numpy(x)
+    x = x.cuda()
+
+    z = np.zeros((2, 512, 1, 1)).astype('float32')
+    z = torch.from_numpy(z)
+    z = z.cuda()
+
+    z_p = m_e(x)
+    x_h = m_d(z_p)
+    rf_f = d_f(z_p.squeeze())
+    x_p = m_d(z)
+    z_h = m_e(x_p)
+    rf_i = d_i(x_p)
+
+    print(
+        'x_h size: {}'.format(x_h.shape),
+        'z_p size: {}'.format(z_p.shape),
+        'rf_f size: {}'.format(rf_f.shape),
+        'z_h size: {}'.format(z_h.shape),
+        'x_p size: {}'.format(x_p.shape),
+        'rf_i size: {}'.format(rf_i.shape),
+        sep='\n'
+    )
